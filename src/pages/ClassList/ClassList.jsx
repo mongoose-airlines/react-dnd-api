d2ae6618cbeb5352090f91fbd77e520e40ef8b08
ManliestBen <benjamintmanley@gmail.com>
2020-09-06 22:27:34 -0500

    add link to classes

--- src/pages/ClassList/ClassList.jsx
+++ src/pages/ClassList/ClassList.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import * as API from '../../services/api-calls'
+import { Link } from 'react-router-dom'
+
 
 class ClassList extends Component {
   state = {
-    classData : [] 
+    results : [] 
   }
 
   async componentDidMount() {
@@ -11,30 +13,27 @@ class ClassList extends Component {
     // and then run setState to update state with the info returned 
     // from the API  (Leave these comments here for now, IMO)
     const classData = await API.getClassList()
-    console.log(classData)
-  this.setState({classData}) 
-    // this doesn't seem right to me?
-    // shouldn't it be like....
-    // this.setState((state) => {
-      //  return {results: classData.results}
-    //})
-    // and then also rename classData to results 
-    // since that's what we get back from the API
+    this.setState({ results: classData.results}) 
+    console.log(this.state.results)
+    // leave this one, just so we can access it
 
-    // brb
-
-    // what is going on in App lol?
-    // it's working
-    // we have state being set.  Just need to make an adjustment
-    // go load it and check the console
-    // App.js is wildly broken on my end lol....
-    // ORLY?  Lemme push
-    
-  }
+    //** each result has index, name, and url classTitle.url I think that you're right, keep going sorry lol
+  } // Now we need to add these links to our App.js Route.
+  // Lemme push rq
   render() {
     return(
       <>
-        <h1>DERP, I'M THE CLASS DATA COMPONENT</h1>
+        <h1>DERP, I'M THE CLASS DATA COMPONENT</h1> 
+        { this.state.results.map((classTitle) => 
+          <Link
+            key={classTitle.name}
+            to={{
+              pathname: `/class/${classTitle.index}`,
+              state: {classTitle}
+            }}
+          >{classTitle.name}               
+          </Link>
+        )}
       </>
     )
   }
