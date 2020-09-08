6be74da89b9c27a5e586349a5c32f912d543b0dd
DavidStinson <mail@davidstinson.dev>
2020-09-07 21:59:34 -0500

    build for heroku

---
+++ build/precache-manifest.9d6e3601e06d944a2697e33d3bb797a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc88b671d1976d376ec0bc73fa09982b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e1b316b674a2fe0467d",
+    "url": "/static/css/main.1034f642.chunk.css"
+  },
+  {
+    "revision": "000c73549a28a36be494",
+    "url": "/static/js/2.f390a75c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f390a75c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e1b316b674a2fe0467d",
+    "url": "/static/js/main.8a07784e.chunk.js"
+  },
+  {
+    "revision": "79e76b8a526f5deb830d",
+    "url": "/static/js/runtime-main.883b2190.js"
+  },
+  {
+    "revision": "d007321734339049f6cf68fa8ed01346",
+    "url": "/static/media/logo.d0073217.svg"
+  }
+]);
